7105abe97687a7d12ea10b492f1e44745441ebbc
Jared LeBlanc <LEBLANCJ@ETSU.EDU>
2026-02-21 18:18:27 -0500

    I dont understand

---
+++ WarGame/WarGame.Core/GameLogic/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarGame.Core.GameLogic
+{
+    public class Card
+    {
+        public Suit Suit { get; }
+        public Rank Rank { get; }
+
+        public Card(Suit suit, Rank rank)
+        {
+            Suit = suit;
+            Rank = rank;
+        }
+
+    }
+}
---
+++ WarGame/WarGame.Core/GameLogic/Suit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarGame.Core.GameLogic
+{
+    public enum Suit
+    {
+        Hearts, Diamonds, Clubs, Spades
+    }
+}
